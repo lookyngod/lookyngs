6b6bc07d3e143f570a33493c4a8ab199faec29a9
lookyngod <pdr.nicolas@gmail.com>
2020-07-18 15:27:06 -0300

    salve atualização

---
+++ js/main.js
@@ -0,0 +1,94 @@
+function funcaoChange(elemento){
+    console.log (elemento.value)
+}
+
+function Facebook(){
+    window.open("https://www.facebook.com/pedro.nikollaz/");
+    //window.location.href = "https://www.facebook.com/pedro.nikollaz/";
+}
+
+function clicou(){
+    alert("Seu lindo, clica mais");
+}
+
+
+
+
+/*
+
+
+
+/*var nome = prompt ("Qual seu nome?")
+if (nome = "Pedrolas"){
+    alert("Monstro Demais!!");
+   
+}
+
+*/
+
+
+
+/*function soma (n1, n2){
+    return n1 + n2;
+}
+
+function setReplace(frase, nome){
+   return frase.replace(nome, novo_nome)
+}
+
+alert(soma(5, 10));
+*/
+
+
+
+
+
+//var d = new Date();
+//alert (d.getMinutes());
+
+
+/*
+var count;
+for(count=0; count <=5; count++){
+    alert (count);
+};
+*/
+
+
+
+
+
+
+
+/*
+var count = 0;
+while (count <= 5) {
+    console.log (count);
+    count = count + 1;
+}
+
+*/
+
+
+
+/*var idade = prompt("Qual sua idade?")
+if (idade >= 23){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+*/
+
+
+
+
+/*
+var nome = "Pedrinho Nicolas";
+var idade = 23 ;
+var frase = "vai chelsão";
+var lista = ("camisa", "chuteira", "meião");
+
+console.log (lista);
+console.log(lista.toString()(0)
+console.log (nome + frase)
+*/
